f2b45cfeba72c96b4697c034139ed3b9e9bb34d7
Subhendu Adhikari <89163743+Subhendu0802@users.noreply.github.com>
2023-07-12 01:09:22 +0530

    day 4

---
+++ rock_paper_scissor.py
@@ -0,0 +1,50 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+graphic=[rock,paper,scissors]
+user_choice=int(input("What will you choose? 0 for rock, 1 for paper, 2 for scissors \n"))
+if(user_choice>=3 or user_choice<0):
+  print("Wrong choice")
+else:
+  print(f"user chooses:- \n {graphic[user_choice]}")
+  computer_choice=random.randint(0,2)
+  print(f"computer chooses:-\n {graphic[computer_choice]}")
+  
+  if(user_choice==2 and computer_choice==0):
+    print("You loose")
+  elif(user_choice==0 and computer_choice==2):
+    print("You Win")
+  elif(user_choice>computer_choice):
+    print("You win")
+  elif(user_choice<computer_choice):
+    print("You loose")
+  elif(user_choice==computer_choice):
+    print("Game Tie")
+  
+    
